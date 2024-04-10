cbe11332cde70b12430ade62b9a5ea26b0a57ebb
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-04-10 10:37:52 +0530

    10-04-24 exam

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// // qus 1
+
+// let c=+prompt("enter celcius");
+// let f=0;
+
+// f=(c*9/5)+32;
+// console.log("celcius converted to fahrenheit :",f);
+
+
+
+
+// // qus 2 
+
+// let basesalary=+prompt("enter base salary");
+// let HRA=+prompt("enter HRA");
+// let DA=+prompt("enter DA");
+// let TA=+prompt("enter TA");
+// let grosssalary=0;
+
+//  HRA=basesalary*HRA/100;
+//  DA=basesalary*DA/100;
+//  TA=basesalary*TA/100;
+
+//  grosssalary=basesalary+HRA+DA+TA;
+//  console.log("Gross salary is :",grosssalary);
+
+
+// // qus 3 
+
+// let firstangle=+prompt("enter first angle");
+// let secondangle=+prompt("enter second angle");
+// let thirdangle=180-(firstangle+secondangle);
+// console.log("Third angle of triangle :",thirdangle);
+
